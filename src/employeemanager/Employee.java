c6e459f8b1b643cdcad7d1f42257325e9779c241
CarlM <carl.daniel.marshall@gmail.com>
2018-09-13 10:39:34 +0200

    Employee id Getter

--- src/employeemanager/Employee.java
+++ src/employeemanager/Employee.java
@@ -1,12 +1,17 @@
 
 package employeemanager;
-public class Employee {
+public abstract class Employee {
+    
     static int id;
+
+    public static int getId() {
+        return id;
+    }
     private String name;
     private int age;
     private double hourlyRate;
     private char gender;
     private double bonus;
-          
     
+    abstract void calculateSalary ();
 }
